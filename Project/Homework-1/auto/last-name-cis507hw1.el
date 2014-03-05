6117c77320fe1a4dcbcadabcb11f782ef938027b
Abraham Xiao <q289196573@gmail.com>
2014-03-05 23:35:45 +0400

    None shall pass. In some video games, this is a script for a hero (or a role) called Black Knight. No matter how hard this semester could be, I shall fight even harder or I would be dismissed from this vocation with a base salary of 6,000 RMB.

---
+++ Project/Homework-1/auto/last-name-cis507hw1.el
@@ -0,0 +1,21 @@
+(TeX-add-style-hook "last-name-cis507hw1"
+ (lambda ()
+    (LaTeX-add-labels
+     "eq:indicator-random-variable")
+    (TeX-run-style-hooks
+     "xcolor"
+     "color"
+     "tikz"
+     "algorithm"
+     "algorithmic"
+     "noend"
+     "amsthm"
+     "amsmath"
+     "amssymb"
+     "epsfig"
+     "latexsym"
+     "latex2e"
+     "art10"
+     "article"
+     "")))
+
---
+++ Project/auto/last-name-cis507hw1.el
@@ -0,0 +1,19 @@
+(TeX-add-style-hook "last-name-cis507hw1"
+ (lambda ()
+    (TeX-run-style-hooks
+     "xcolor"
+     "color"
+     "tikz"
+     "algorithm"
+     "algorithmic"
+     "noend"
+     "amsthm"
+     "amsmath"
+     "amssymb"
+     "epsfig"
+     "latexsym"
+     "latex2e"
+     "art10"
+     "article"
+     "")))
+
