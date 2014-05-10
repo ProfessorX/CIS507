d705c4fec9d7096a0fda1f48e103b910d6688b15
Abraham Xiao <q289196573@Gmail.com>
2014-05-11 00:19:27 +0400

    Life is awesome! Miss Azziza Al Sawafi once told me to be happy, even I may be in the lowest part of my life. She said, with the work station and working environment provided by Masdar Institute, or Abu Dhabi government, you should be happy. No other institute is comparable, at least those in China. The last sentence is from my heart. A hah.

---
+++ Project/Homework-3/auto/xiao-cis507hw3.el
@@ -0,0 +1,16 @@
+(TeX-add-style-hook "xiao-cis507hw3"
+ (lambda ()
+    (TeX-run-style-hooks
+     "xcolor"
+     "color"
+     "tikz"
+     "amsthm"
+     "amsmath"
+     "amssymb"
+     "epsfig"
+     "latexsym"
+     "latex2e"
+     "art10"
+     "article"
+     "")))
+
